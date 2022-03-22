c73ddb9f5e72804f012ce2347d1f4a5933330c6c
qowldud95 <girlwldud@naver.com>
2022-03-22 23:22:59 +0900

    20220322

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,44 @@
-alert("hi");
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✔️");
+});
+
+socket.addEventListener("message", (message) =>{
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌")
+});
+
+setTimeout(()=>{
+    socket.send("hello from the browser!");
+}, 10000);
+
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    console.log(input.value);
+}
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
